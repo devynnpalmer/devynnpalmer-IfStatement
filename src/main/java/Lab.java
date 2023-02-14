b5eb31754cce065bd121a5101341f431f98b8fd3
Devynn R. Palmer <palmerdevynn@gmail.com>
2023-02-14 22:31:55 +0000

    If Statement

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,6 +20,11 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+        if(bool == true) {
+            return x;
+        } else if(bool == false) {
+            return y;
+        }
         return 0;
     }
 }
